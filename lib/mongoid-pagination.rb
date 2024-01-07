75f0a359a0afff000f112fd96686eced9c3d3198
Luis Castillo <luis.castillo@asap507.com>
2024-01-07 06:04:14 -0600

    add pagination helpers

--- lib/mongoid-pagination.rb
+++ lib/mongoid-pagination.rb
@@ -1,8 +1,8 @@
 require 'mongoid'
-require 'mongoid/helpers/link_tag'
 require 'mongoid/helpers/pagination_helper'
-require 'mongoid/helpers/paginator'
-require 'mongoid/helpers/link_tag.rb'
 require 'mongoid/pagination'
 
-ActionView::Base.send :include, Mongoid::Helpers
+if defined? ::Rails::Railtie
+    require 'mongoid/engine'
+end
+  
